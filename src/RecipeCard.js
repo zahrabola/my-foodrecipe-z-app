2488522e80fc02f0ea8c6328b9a084f1018ee54d
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-06-07 15:13:23 +0100

    Update Recipe card

---
+++ src/RecipeCard.js
@@ -0,0 +1,22 @@
+import React from "react";
+const RecipeCard = ({}) =>{
+return (
+  <div className="col">
+    <div className="card">
+      <img src="..." class="card-img-top" alt="..." />
+      <div className="cardbody">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">Category</p>
+        <a href="#" class="btn btn-primary">
+ Modal
+        </a>
+      </div>
+    </div>
+  </div>
+);
+}
+
+
+
+
+export default RecipeCard;
